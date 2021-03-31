0da4e193ef6824e21ade413d7b6467d7d6927325
Jhonnel Nuñez <sistemasyserviciosnunez@gmail.com>
2021-03-31 15:24:33 -0400

    Agregadas secciones para colocar funcion que asigne lead, el modelo debe ser revisado tiene algunos errores

--- models/grupo_agentes.py
+++ models/grupo_agentes.py
@@ -1,8 +1,34 @@
 from odoo import models, fields
+# Se creo un modelo para segmentar a los agentes a los que se les asignara los leads
+# El cual agrega un campo nuevo a la tabla res_user
+#
+# Queda pentiene la funcion que asigna el lead
 
+
+# Clase creada para segmentar a los agentes a los cuales se les asignan los leads
+class GrupoAgentes(models.Model):   
+    grupo_agentes = fields.Char(string="grupo de agentes asigna leads", required=True)
 
 class ResUsers(models.Model):
     _inherit = "res.users"
     
-    grupo_agentes_venta = fields.Char(string="grupo de agentes asigna leads", required=False)
+    grupo_agentes = fields.Many2one(GrupoAgentes, string="grupo de agentes asigna leads", on_delete=models.CASCADE, required=False)
+
     
+class CrmLead(models.Model):
+    _inherit = 'crm.lead'
+
+    @api.model
+    def create(self, values): # Aqui se agrega el agente asignado al lead
+        agente = values.get('user_id')
+        
+        if agente:
+            agente = self.asigna_agente()
+        
+        return agente
+
+    def asigna_agente(): # Aqui va la funcion que asigna el agente al lead
+     
+        # Aqui va la funcion que asigna el agente al lead
+
+        return agente
