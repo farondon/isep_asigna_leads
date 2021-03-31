69587cefb722122980f58513dd92a1bb553426d4
Jhonnel Nuñez <sistemasyserviciosnunez@gmail.com>
2021-03-31 11:12:30 -0400

    Inicio de desarrollo modulo asignador de leads

---
+++ __manifest__.py
@@ -0,0 +1,7 @@
+{ 
+    'name': 'Asigna leads', 
+    'description': 'Asigna leads automaticamente a agentes de ventas en funcion de disponibilidad y tasa de conversión', 
+    'author': 'Jhonnel Nuñez, Farjan Rondón', 
+    'depends': ['base','crm'], 
+    'application': True, 
+}
